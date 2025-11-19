1cf6765f2a06e56d2e6b63893066650b6ca145de
RobinVW89 <rvnppjydhj@privaterelay.appleid.com>
2025-11-19 21:37:07 +0000

    Suppression de l'adaptateur Cloudflare pour site statique pur

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.conseilfiscalauto.com",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
 });
